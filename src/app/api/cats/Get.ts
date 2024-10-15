f81ba1eaa5b0af6c3f0219784e7a4fcfdb8d2dbd
tomominakasu@gmail.com <tomominakasu@gmail.com>
2024-10-15 11:34:33 +0800

    change data structure

--- src/app/api/cats/Get.ts
+++ src/app/api/cats/Get.ts
@@ -1,10 +1,18 @@
 import { NextResponse } from "next/server";
 
 
+
 const GetHandler = async() => {
-    const data = {
-        message: "Hello from API",
-    };
+    const data = [
+      {
+        cat_id: 1,
+        cat_name: "JIJI",
+      },
+      {
+        cat_id: 2,
+        cat_name: "ChaChaMaru",
+      },
+    ];
     return NextResponse.json(data);
 }
 
